2f5fdb72d2cc6aa03af66000af6409668452f7bc
Noor696 <eng.nooralhateeb@gmail.com>
2023-01-15 14:37:22 +0000

    creat image field on post

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ibrahimalmanaseer/hm_tour/Backend/.venv"
+set -gx VIRTUAL_ENV "/home/noor/AdvPy-401/finalProject/Backend/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- post/models.py
+++ post/models.py
@@ -10,9 +10,24 @@ class Post(models.Model):
     description=models.TextField(blank=True)
     date=models.DateTimeField(auto_now_add=True)
     rate=models.IntegerField(default=5)
-    image=models.URLField(blank=False)
+    image=models.ImageField(null=True, blank=True)
     location=models.TextField()
     price=models.FloatField()
 
+# python -m pip install Pillow  -> run this command to accept the image
+
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ["-pk"]
+
+    @property
+    def imageURL(self):
+
+        try:
+            url = self.image.url
+        except:
+            url = 'https://www.travel-intel.com/wp-content/uploads/2022/05/Tourism-takes-off-summer-2022.jpg'
+            
+        return url
