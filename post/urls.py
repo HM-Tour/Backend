78a808f2fa41c23a77b883369c8ef656549dd60c
MohdHanoti <mohadalhanoty@gmail.com>
2023-01-17 12:04:08 +0000

    new feat from mohammad alhanoti:creat full post

--- post/urls.py
+++ post/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('',PostListView.as_view(), name='posts'),
-    path('<int:pk>', PostDetailView.as_view(),name='post')
+    path('<int:pk>', PostDetailView.as_view(),name='post'),
+    path('upload', ImageUploadView.as_view()),
+    path('fetch-images', GetImagesView.as_view()),
 ]
--- post/views.py
+++ post/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers  import PostSerializer
 
@@ -19,4 +22,53 @@ class PostDetailView(RetrieveUpdateDestroyAPIView):
     queryset=Post.objects.all()
     serializer_class= PostSerializer
 
+class GetImagesView(APIView):
+    def get(self, request, format=None):
+        if Post.objects.all().exists():
+            images = Post.objects.all()
+            images = PostSerializer(images, many=True)
 
+            return Response(
+                {'images': images.data},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {'error': 'No images found'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+
+class ImageUploadView(APIView):
+    def post(self, request):
+        try:
+            data = self.request.data
+
+            image = data['image']
+            
+            title = data['Title']
+            description = data['Description']
+            date = data['Date']
+            rate = data['Rate']
+            location = data['Location']
+            price = data['Cost']
+
+            Post.objects.create(
+                image=image,
+                title=title,
+                description=description,
+                date=date,
+                rate=rate,
+                location=location,
+                price=price,
+            )
+
+            return Response(
+                {'success': 'Successfully uploaded image'},
+                status=status.HTTP_201_CREATED
+            )
+        except:
+            return Response(
+                {'error': 'Something went wrong when uploading image'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
