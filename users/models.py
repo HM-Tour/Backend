0bb3196f76e6dfda98800ae7ca599f806b2b0f2b
Mostafa-Albelbeisi <mostfabelbeisi@gmail.com>
2023-01-21 13:14:44 +0000

    fix users

--- users/models.py
+++ users/models.py
@@ -9,6 +9,6 @@ class CustomUser(AbstractUser):
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    description = models.CharField(max_length=255, default=False, unique=False)
-    location = models.CharField(max_length=255, default=False, unique=False)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
     username = models.CharField(max_length=255, unique=True)
