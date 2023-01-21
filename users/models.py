e4e846175d915c217ad953480716e8f7ea50c3dd
Mostafa-Albelbeisi <mostfabelbeisi@gmail.com>
2023-01-21 11:25:48 +0000

    fix profile Mostafa

--- users/models.py
+++ users/models.py
@@ -6,7 +6,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+    description = models.CharField(max_length=255, default=False, unique=False)
+    location = models.CharField(max_length=255, default=False, unique=False)
     username = models.CharField(max_length=255, unique=True)
